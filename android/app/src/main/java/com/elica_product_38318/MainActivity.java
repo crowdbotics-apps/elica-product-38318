40872740722f2e4a4a02504ee40e34716d5bde35
Crowdbotics <team@crowdbotics.com>
2023-01-16 18:31:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/elica_product_38318/MainActivity.java
@@ -0,0 +1,15 @@
+package com.elica_product_38318;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "elica_product_38318";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "elica-product-38318",
+  url: "https://elica-product-38318.botics.co",
+  api: "https://elica-product-38318.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
